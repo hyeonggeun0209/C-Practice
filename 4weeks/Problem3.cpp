2444318f0cc9437acabfa8deddbcfc6907dde26e
hyeonggeun0209 <55240146+hyeonggeun0209@users.noreply.github.com>
2019-10-01 16:11:18 +0900

    Rename Problem3.cpp to 4weeks/Problem3.cpp

--- 4weeks/Problem3.cpp
+++ 4weeks/Problem3.cpp
@@ -38,12 +38,12 @@ int main() {
 	MyIntStack a;
 	for (int i = 0; i < 11; i++) {
 		if (a.push(i)) cout << i << ' ';
-		else cout << endl << i + 1 << " ��° stack full" << endl;
+		else cout << endl << i + 1 << " ¹øÂ° stack full" << endl;
 	}
 	int n;
 	for (int i = 0; i < 11; i++) {
 		if (a.pop(n)) cout << n << ' ';
-		else cout << endl << i + 1 << " ��° stack empty";
+		else cout << endl << i + 1 << " ¹øÂ° stack empty";
 	}
 	cout << endl;
 }
